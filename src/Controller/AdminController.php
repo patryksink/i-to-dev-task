e2bf0f605452822776d6c350931c0c730d20b18d
patryksink <patrycjus.august.sinkevic@gmail.com>
2022-05-07 21:27:50 +0300

    Register form handling and display

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/', name: '')]
     public function index(UserRepository $userRepository): Response
     {
         $users = $userRepository->findAll();
@@ -19,5 +25,33 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    #[Route('/user/add', name: '_user_add')]
+    public function registerUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        $isFromValid = $form->isValid();
+        if (!$form->isSubmitted() || !$isFromValid) {
+            return $this->render('admin/user_register.html.twig', [
+                'registrationForm' => $form->createView(),
+                'is_form_valid' => $isFromValid
+            ]);
+        }
+
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            )
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin');
     }
 }
