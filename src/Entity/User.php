3e4e58e67b2cfe93fbcee51b74db66ae8b9b0364
patryksink <patrycjus.august.sinkevic@gmail.com>
2022-05-07 21:26:39 +0300

    Added asserts on User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -19,15 +22,23 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotNull(message: "Name cannot be null")]
+    #[Assert\NotBlank(message: "Please enter a name")]
+    #[Assert\Length(max: 255, maxMessage: 'Your email should be not be longer than {{ limit }} characters')]
     private ?string $name;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Assert\Email(message: "Please enter valid email")]
+    #[Assert\NotNull(message: "Email cannot be null")]
+    #[Assert\NotBlank(message: "Please enter a email")]
+    #[Assert\Length(max: 180, maxMessage: 'Your email should be not be longer than {{ limit }} characters')]
     private ?string $email;
 
     #[ORM\Column(type: 'json')]
     private array $roles = [];
 
     #[ORM\Column(type: 'string')]
+    #[Assert\Length(min: 6, max: 4096, minMessage: 'Your password should be at least {{ limit }} characters', maxMessage: 'Your password should be not be longer than {{ limit }} characters')]
     private string $password;
 
     public function getId(): ?int
