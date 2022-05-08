4298f3af3dd3baf22671c9b80d439fc005db40e2
patryksink <patrycjus.august.sinkevic@gmail.com>
2022-05-08 15:54:06 +0300

    Added mail sending subscriber after user register

---
+++ src/DoctrineSubscriber/PostPersistSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DoctrineSubscriber;
+
+use App\Entity\User;
+use App\Security\EmailVerifier;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
+
+class PostPersistSubscriber implements EventSubscriber
+{
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist => 'postPersist',
+        ];
+    }
+
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof User) {
+            $this->emailVerifier->sendEmailConfirmation('verify_email', $entity,
+            (new TemplatedEmail())
+                ->from(new Address('test@test.com', 'Test'))
+                ->to($entity->getEmail())
+                ->subject('Please Confirm your Email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+        }
+    }
+}
