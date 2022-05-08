4ce933f4e354c9bd90a87dfaffea6ed58926f151
patryksink <patrycjus.august.sinkevic@gmail.com>
2022-05-08 15:49:23 +0300

    user login repository add next feature

---
+++ src/Repository/UserLoginRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\UserLogin;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method UserLogin|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserLogin|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserLogin[]    findAll()
+ * @method UserLogin[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserLoginRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserLogin::class);
+    }
+}
